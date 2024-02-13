b43b45b992d4da9a8520e42207ef0b160457b228
David-F-98 <u2903149@unimilitar.edu.co>
2024-02-13 16:41:48 -0500

    slide botones

--- src/Components/Anuncios.jsx
+++ src/Components/Anuncios.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { useRef } from 'react';
 import img1 from './../img/1.jpg';
 import img2 from './../img/2.jpg';
 import img3 from './../img/3.jpg';
 import styled from 'styled-components';
 
 const Anuncios = () => {
+    const slideshow = useRef(null);
+
+    const siguiente = ()=>{
+        if(slideshow.current.children.length > 0){
+            
+            const primerElemento = slideshow.current.children[0];
+            slideshow.current.style.transition = `800ms ease-out all`;
+
+            const tamanoSlide = slideshow.current.children[0].offsetWidth;
+
+
+            slideshow.current.style.transform = `translateX(-${tamanoSlide}px)`;
+
+            const transicion = ()=>{
+                slideshow.current.style.transition = 'none';
+                slideshow.current.style.transform = `translateX(0)`;
+
+                slideshow.current.appendChild(primerElemento);
+            }
+
+            slideshow.current.addEventListener('transitionend',transicion);
+        }
+    };
+    
+    const anterior = ()=>{
+        console.log(slideshow.current);
+    };
+
     return ( 
       <ContenedorPrincipal>
-        <ContenedorSlideShow>
+        <ContenedorSlideShow ref={slideshow}>
             <Slide>
                 <a href="#">
                     <img src={img1} alt="" />
@@ -25,10 +54,10 @@ const Anuncios = () => {
             </Slide>
         </ContenedorSlideShow>
         <Controles>
-            <Boton >
+            <Boton onClick={anterior}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>
             </Boton>
-            <Boton $derecho={true}>
+            <Boton $derecho={true} onClick={siguiente}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.33 24l-2.83-2.829 9.339-9.175-9.339-9.167 2.83-2.829 12.17 11.996z"/></svg>
             </Boton>
         </Controles>
